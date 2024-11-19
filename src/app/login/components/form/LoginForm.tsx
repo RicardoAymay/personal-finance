2f4648a4c5bf73378558e92871b344288c69cb41
RicardoAymay <r.aymay.goncalves@gmail.com>
2024-11-19 12:12:32 -0300

    login form ready

--- src/app/login/components/form/LoginForm.tsx
+++ src/app/login/components/form/LoginForm.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import Input from '../input/Input'
 import Link from 'next/link'
 import { SubmitHandler, useForm } from 'react-hook-form'
@@ -7,8 +7,8 @@ import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 
 const LoginSchema = z.object({
-    'loginUserEmail': z.string().email({message:"Invalid data format"}).min(5, {message: "E-mail is too short"}).trim().toLowerCase(),
-    'loginUserPassword': z.string()
+    'userEmail': z.string().email({message:"Invalid data format"}).min(5, {message: "E-mail is too short"}).trim().toLowerCase(),
+    'userPassword': z.string()
 })
 
 interface FormValues {
@@ -19,11 +19,13 @@ userPassword: string,
 const LoginForm = () => {
     const {register, handleSubmit, formState: {errors}} = useForm<FormValues>({resolver: zodResolver(LoginSchema), mode: 'onBlur'})
     const onSubmit: SubmitHandler<FormValues> = (data) => {console.log(data)}
+    const [visible, setVisible] = useState<boolean>(false)
+
   return (
     <form  className="flex flex-col flex-1"  onSubmit={handleSubmit(onSubmit)}>
         <Input labelText='Email' id='loginUserEmail' {...register("userEmail")} />
-        <Input labelText='Password' id='loginUserPassword' {...register("userPassword")} />
-        <button className='h-[53px] mt-6 bg-grey-900 text-white text-preset-4-bold rounded-lg flex items-center justify-center'>Login</button>
+        <Input labelText='Password' visible={visible} setVisible={setVisible} type={visible ? "text" : "password"} id='loginUserPassword' icon {...register("userPassword")} />
+        <button type='submit' className='h-[53px] mt-6 bg-grey-900 text-white text-preset-4-bold rounded-lg flex items-center justify-center'>Login</button>
         <p className='text-center mt-8 text-preset-4 text-grey-500'>Need to create an account? <Link className='text-grey-900 text-preset-4-bold underline' href={"/signup"}> Sign up</Link></p>
     </form>
   )
--- src/app/login/components/input/Input.tsx
+++ src/app/login/components/input/Input.tsx
@@ -1,18 +1,52 @@
-import { forwardRef, InputHTMLAttributes } from "react"
-
+import { Dispatch, forwardRef, InputHTMLAttributes, SetStateAction } from "react"
+import hide from "../../../../assets/images/icon-hide-password.svg"
+import show from "../../../../assets/images/icon-show-password.svg"
+import Image from "next/image";
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
     labelText?: string;
-    icon?: string
+    icon?: boolean
     colorTag?: string
+    visible?: boolean
+    setVisible?: Dispatch<SetStateAction<boolean>>;
   }
-
-const Input = forwardRef<HTMLInputElement, InputProps> (({name = '', labelText, type = 'text', id, ...props}, ref) => {
-  return (
-    <label className="flex flex-col mb-2 mt-2" htmlFor={id}>
-        <span className="text-preset-5-bold text-grey-500 mb-1">{labelText}</span>
-        <input className="border border-beige-500 h-[45px] rounded-lg indent-2" type={type} name={name} id={id} ref={ref} {...props}></input>
-    </label>
-  )
-})
+  const Input = forwardRef<HTMLInputElement, InputProps> (({name, visible, setVisible, labelText, type, icon, id, ...props}, ref) => {
+    const handleVisibility = () =>{
+      if (setVisible){
+        setVisible(!visible)
+      }
+    }
+    return (
+      <fieldset className="flex flex-col">
+        {labelText && (
+          <label className="mb-2 mt-2" htmlFor={id}>
+            <span className="text-preset-5-bold text-grey-500 mb-1">{labelText}</span>
+          </label>
+        )}
+        <div className="w-full flex relative">
+          <input
+            className="border border-beige-500 h-[45px] rounded-lg indent-2 w-full pr-10"
+            type={visible ? "text" : "password"} // Dynamically set input type
+            name={name}
+            id={id}
+            ref={ref}
+            {...props}
+          />
+          {icon && (
+            <div
+              className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer z-10"
+              onClick={handleVisibility}
+            >
+              <Image
+                className="h-5 w-5"
+                src={visible ? show : hide} // Dynamically update the image source
+                alt={visible ? "Hide Password" : "Show Password"}
+              />
+            </div>
+          )}
+        </div>
+      </fieldset>
+    );
+  }
+);
 
 export default Input
